e21ad9e0cbb3cef3f62f6b443c4be8893e9336c7
K9-98 <k9idas@gmail.com>
2017-09-19 12:11:58 +0200

    commit removed banner
    removed banner from login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template:`
-  <ahn-banner></ahn-banner>
-  <router-outlet></router-outlet>
-  `,
-  //templateUrl: './app.component.html',
+  templateUrl:'./app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {

32830292a602d3e7e7ac34df18772b722225c71b
K9-98 <k9idas@gmail.com>
2017-09-19 11:45:18 +0200

    Commit 1
    added tabbed panes

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -1,10 +1,17 @@
+
+import { TabComponent } from './../tab/tab.component';
+import { TabsComponent } from './../tab/tabs.component';
 import { Component, OnInit } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.css']
 })
+
+
 export class StatisticsComponent implements OnInit {
 
   constructor() { }

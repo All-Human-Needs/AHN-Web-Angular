8a89509b0859fe60479562094c1cedc7ea7a2c0b
K9-98 <k9idas@gmail.com>
2017-09-27 09:45:16 +0200

    Commit 1
    Added mock stats

---
+++ src/app/statistics/dailyStats.ts
@@ -0,0 +1,4 @@
+export class DailyStats{
+    date : Date;
+    numberOfPeople: number;
+}
---
+++ src/app/statistics/mock-daily.ts
@@ -0,0 +1,8 @@
+import { DailyStats } from './dailyStats';
+
+export const DailyStatistcs:DailyStats[] = [
+    {date: new Date(2017,9,17,7,30,0), numberOfPeople:20},
+    {date: new Date(2017,9,17,8,0,0), numberOfPeople:50},
+    {date: new Date(2017,9,17,8,30,0), numberOfPeople:10},
+    {date: new Date(2017,9,17,9,0,0), numberOfPeople:60},
+]

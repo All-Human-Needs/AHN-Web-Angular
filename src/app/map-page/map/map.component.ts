cc1757372d1f94740551864573fd11c54e7b3ac7
gertMinnie <gertminnie3@gmail.com>
2017-09-27 14:55:34 +0200

    map page 0.0.2

--- src/app/map-page/map/map.component.ts
+++ src/app/map-page/map/map.component.ts
@@ -12,4 +12,35 @@ export class MapComponent implements OnInit {
   ngOnInit() {
   }
 
+  initMap() {
+
+    //test variable also place to centre map on initialization
+    var capeTown = {
+      lat: -33.9231502, lng: 18.4472879
+
+    };
+
+    //disabling labels and Points Of interest on map so we can insert our own
+    var map = new google.maps.Map(document.getElementById('map-GM'), {
+      zoom: 13,
+      center: capeTown,
+      styles: [
+        {
+          featureType: "poi",
+          elementType: "labels",
+          stylers: [
+            { visibility: "off" }
+          ]
+        },
+        {
+          featureType: "transit",
+          elementType: "labels",
+          stylers: [
+            { visibility: "off" }
+          ]
+        }
+      ]
+    });
+  }
+
 }

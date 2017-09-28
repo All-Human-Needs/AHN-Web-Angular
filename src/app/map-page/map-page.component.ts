da8ae4506cf36203fd41384dcdf37a9feae0ce3a
gertMinnie <gertminnie3@gmail.com>
2017-09-28 14:40:11 +0200

    v 0.0.5
    got map to work, looking at displaying markers and stuff now

--- src/app/map-page/map-page.component.ts
+++ src/app/map-page/map-page.component.ts
@@ -7,15 +7,55 @@ import { AgmCoreModule } from '@agm/core';
   templateUrl: './map-page.component.html',
   styleUrls: ['./map-page.component.css']
 })
+
 export class MapPageComponent implements OnInit {
 
   title: string = 'My first AGM project';
-  lat: number = 51.678418;
-  lng: number = 7.809007;
+  lat: number = -33.924632;
+  lng: number = 18.429371;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  locations = [
+    {
+        lat: -33.9217137,
+        lng: 18.46171849999996,
+        name: 'Groote Schuur Hospital',
+    }, {
+        lat: -33.9423756,
+        lng: 18.463247300000035,
+        name: 'Netcare UCT Private Academic Hospital',
+    }, {
+        lat: -33.9443605,
+        lng: 18.49009860000001,
+        name: 'Life Vincent Pallotti Hospital',
+    }, {
+        lat: -33.931401,
+        lng: 18.45506899999998,
+        name: 'Nedbank Salt River Cape',
+    }, {
+        lat: -33.923951,
+        lng: 18.422884000000067,
+        name: 'Nedbank Grand Central',
+    },
+    {
+        lat: -33.91724,
+        lng: 18.42728599999998,
+        name: 'Nedbank Foreshore',
+    }, {
+        lat: -33.92765,
+        lng: 18.4221,
+        name: 'Department of Home Affairs Cape Town',
+    }, {
+        lat: -33.9012178,
+        lng: 18.63154910000003,
+        name: 'Department of Home Affairs Office',
+    }, {
+        lat: -34.0032484,
+        lng: 18.4700239,
+        name: 'Home Affairs Wynberg District',
+    }]
 }

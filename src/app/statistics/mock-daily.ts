b5915d87772d14cbffd96af725805c5fb693f4fb
K9-98 <k9idas@gmail.com>
2017-09-29 11:20:16 +0200

    commit stats
    used mock data from service to populate graphs

--- src/app/statistics/mock-daily.ts
+++ src/app/statistics/mock-daily.ts
@@ -1,8 +1,11 @@
-import { DailyStats } from './dailyStats';
+import { DailyFootTraffic } from './foot-traffic-stats';
 
-export const DailyStatistcs:DailyStats[] = [
+
+export const DailyStatistics:DailyFootTraffic[] = [
+    {date: new Date(2017,9,17,7,0,0), numberOfPeople:0},
     {date: new Date(2017,9,17,7,30,0), numberOfPeople:20},
     {date: new Date(2017,9,17,8,0,0), numberOfPeople:50},
     {date: new Date(2017,9,17,8,30,0), numberOfPeople:10},
     {date: new Date(2017,9,17,9,0,0), numberOfPeople:60},
+    {date: new Date(2017,9,17,17,0,0), numberOfPeople:0},
 ]

f86ad925b7df7224c14628bdc2f8dd72905f043e
K9-98 <k9idas@gmail.com>
2017-10-02 08:14:05 +0200

    commit 8
    added background image

--- src/app/statistics/bar-graph/bar-graph.component.ts
+++ src/app/statistics/bar-graph/bar-graph.component.ts
@@ -11,15 +11,32 @@ import { Component, OnInit } from '@angular/core';
 export class BarGraphComponent implements OnInit {
   dailyStatistics: DailyFootTraffic[];
   numPeople:number[]=[];
+  //barchart initialisation
+  barChartData:any[]=[{data:[],label:''}]; 
+  barChartLabels:any[]=[]; 
+  barChartOptions:any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  
+  barChartType:string = 'bar';
+  barChartLegend:boolean = true;
+  barChartColors:Array<any> = [
+   { 
+     hoverBackgroundColor:'#fff',
+     backgroundColor:'#000',
+   },  
+  ];
+
   constructor(private dailyService : StatisticsService) { }
 
+  //populate bar chart 
   getDailyData(){
    
     this.dailyService.getDailyStatistics().then(dailyStatistics => {
       
       this.dailyStatistics = dailyStatistics;
       let day:any =this.dailyStatistics[0].date.getDay();
-      console.table(this.dailyStatistics);
       for(var i = 0;i<this.dailyStatistics.length;i++){
         let hours:any = this.dailyStatistics[i].date.getHours();
         let min:any = this.dailyStatistics[i].date.getMinutes();
@@ -29,8 +46,7 @@ export class BarGraphComponent implements OnInit {
         this.numPeople[i] = this.dailyStatistics[i].numberOfPeople;
        
       }
-      //  console.log(this.numPeople);
-      //  console.log(this.barChartLabels);
+  
       this.barChartData = [
         {data : this.numPeople, label : 'Tuesday'}
       ];
@@ -39,36 +55,8 @@ export class BarGraphComponent implements OnInit {
 
 
   ngOnInit() {
-    //this.dailyService.getDailyStatistics().then(x => console.log(x));
     this.getDailyData();
   }
-
-
-  public barChartOptions:any = {
-    scaleShowVerticalLines: false,
-    responsive: true
-  };
-  
-  public barChartType:string = 'bar';
-  public barChartLegend:boolean = true;
-  public barChartColors:Array<any> = [
-    { 
-      hoverBackgroundColor:'#fff',
-      backgroundColor:'#000',
-      
-    },
-    
-  ];
-
-
-  public barChartData:any[]=[{data:[],label:''}]; //= [
-  //   {data: [0,12,20,32,56,32,11,45,43,5,12,0], label: 'Monday'}
-  // ];
-   public barChartLabels:any[]=[]; //= [
-  //   '07:00','07:30','08:00','08:30',
-  //   '09:00','09:30','10:00','10:30',
-  //   '11:00','11:30','12:00','12:30'];
- 
  
 }
 

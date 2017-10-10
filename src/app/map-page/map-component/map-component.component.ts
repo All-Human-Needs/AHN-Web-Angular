00bf48a598824c63ec0c7e8875e50f1c74ac8d51
gertMinnie <gertminnie3@gmail.com>
2017-10-10 14:50:41 +0200

    v 0.2.3

--- src/app/map-page/map-component/map-component.component.ts
+++ src/app/map-page/map-component/map-component.component.ts
@@ -1,4 +1,5 @@
 import { BusinessService } from './../../mock-data/business.service';
+import { Business } from './../../mock-data/mock-classes';
 
 import { Component, OnInit } from '@angular/core';
 
@@ -10,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponentComponent implements OnInit {
 
-  title: string = 'My first AGM project';
+  title: string = 'map-blep';
   lat: number = -33.924632;
   lng: number = 18.429371;
 
@@ -36,47 +37,8 @@ export class MapComponentComponent implements OnInit {
     ///////////////////////////////////////////////
   }
 
-  locations:location[] = this.businessService.getBusinesses();
+  locations:Business[] = this.businessService.getBusinesses();
 
-  // locations: location[] = [
-  //   {
-  //     lat: -33.9217137,
-  //     lng: 18.46171849999996,
-  //     name: 'Groote Schuur Hospital',
-  //   }, {
-  //     lat: -33.9423756,
-  //     lng: 18.463247300000035,
-  //     name: 'Netcare UCT Private Academic Hospital',
-  //   }, {
-  //     lat: -33.9443605,
-  //     lng: 18.49009860000001,
-  //     name: 'Life Vincent Pallotti Hospital',
-  //   }, {
-  //     lat: -33.931401,
-  //     lng: 18.45506899999998,
-  //     name: 'Nedbank Salt River Cape',
-  //   }, {
-  //     lat: -33.923951,
-  //     lng: 18.422884000000067,
-  //     name: 'Nedbank Grand Central',
-  //   },
-  //   {
-  //     lat: -33.91724,
-  //     lng: 18.42728599999998,
-  //     name: 'Nedbank Foreshore',
-  //   }, {
-  //     lat: -33.92765,
-  //     lng: 18.4221,
-  //     name: 'Department of Home Affairs Cape Town',
-  //   }, {
-  //     lat: -33.9012178,
-  //     lng: 18.63154910000003,
-  //     name: 'Department of Home Affairs Office',
-  //   }, {
-  //     lat: -34.0032484,
-  //     lng: 18.4700239,
-  //     name: 'Home Affairs Wynberg District',
-  //   }]
 
   customStyle = [
     {
@@ -95,13 +57,4 @@ export class MapComponentComponent implements OnInit {
     }
   ]
 
-}
-
-//just making an interface for type safety sake
-interface location {
-  lat: number;
-  lng: number;
-  name: string;
-  id:string;
-  maxCapacity:number;
 }
--- src/app/mock-data/mock-data.ts
+++ src/app/mock-data/mock-data.ts
@@ -1,61 +1,49 @@
 
-import { FootTraffic, User,Business } from './mock-classes';
+import { FootTraffic, User, Business } from './mock-classes';
 
-export const HourlyStatistics:FootTraffic[] = [
-    {id:"ad02b",date: new Date(2017,9,17,7,0,0), numberOfPeople:0},
-    {id:"ad02b",date: new Date(2017,9,17,7,30,0), numberOfPeople:20},
-    {id:"ad02b",date: new Date(2017,9,17,8,0,0), numberOfPeople:50},
-    {id:"ad02b",date: new Date(2017,9,17,8,30,0), numberOfPeople:10},
-    {id:"ad02b",date: new Date(2017,9,17,9,0,0), numberOfPeople:60},
-    {id:"ad02b",date: new Date(2017,9,17,17,0,0), numberOfPeople:0},
+export const HourlyStatistics: FootTraffic[] = [
+    { id: "001", date: new Date(2017, 9, 17, 7, 0, 0), numberOfPeople: 0 },
+    { id: "001", date: new Date(2017, 9, 17, 7, 30, 0), numberOfPeople: 20 },
+
+    { id: "002", date: new Date(2017, 9, 17, 7, 0, 0), numberOfPeople: 30 },
+    { id: "002", date: new Date(2017, 9, 17, 7, 30, 0), numberOfPeople: 40 },
+
+    { id: "003", date: new Date(2017, 9, 17, 7, 0, 0), numberOfPeople: 10 },
+    { id: "003", date: new Date(2017, 9, 17, 7, 30, 0), numberOfPeople: 20 },
+
+    { id: "004", date: new Date(2017, 9, 17, 7, 0, 0), numberOfPeople: 40 },
+    { id: "004", date: new Date(2017, 9, 17, 7, 30, 0), numberOfPeople: 30 },
+
+    { id: "005", date: new Date(2017, 9, 17, 7, 0, 0), numberOfPeople: 30 },
+    { id: "005", date: new Date(2017, 9, 17, 7, 30, 0), numberOfPeople: 20 },
+
+    { id: "006", date: new Date(2017, 9, 17, 7, 0, 0), numberOfPeople:60 },
+    { id: "006", date: new Date(2017, 9, 17, 7, 30, 0), numberOfPeople: 10 },
+
+    { id: "007", date: new Date(2017, 9, 17, 7, 0, 0), numberOfPeople: 10 },
+    { id: "007", date: new Date(2017, 9, 17, 7, 30, 0), numberOfPeople: 30 },
+
+    { id: "008", date: new Date(2017, 9, 17, 7, 0, 0), numberOfPeople: 30 },
+    { id: "008", date: new Date(2017, 9, 17, 7, 30, 0), numberOfPeople: 40 },
+
+    { id: "009", date: new Date(2017, 9, 17, 7, 0, 0), numberOfPeople: 0 },
+    { id: "009", date: new Date(2017, 9, 17, 7, 30, 0), numberOfPeople: 20 },
 ]
 
-export const Users:User[] = [
-    {id:"ad01c",email:"admin@mail.com",password:"admin"},
-    {id:"ad02b",email:"admin@mail.com",password:"admin"},
+export const Users: User[] = [
+    { id: "ad01c", email: "admin@mail.com", password: "admin" },
+    { id: "ad02b", email: "admin@mail.com", password: "admin" },
 ]
 
-export const RegisteredBusinesses:Business[] = [
-    {id:"ad02b",name:"Tester",lat:0,lng:0,maxCapacity:60},
-    {id:"",name:'Groote Schuur Hospital',lat: -33.9217137,lng: 18.46171849999996,maxCapacity:60}
+export const RegisteredBusinesses: Business[] = [
+    { id: "ad02b", name: "Tester", lat: 0, lng: 0, maxCapacity: 60 },
+    { id: "001", name: 'Groote Schuur Hospital', lat: -33.9217137, lng: 18.46171849, maxCapacity: 60 },
+    { id: "002", name: 'Netcare UCT Private Academic Hospital', lat: -33.9423756, lng: 18.4632473, maxCapacity: 60 },
+    { id: "003", name: 'Life Vincent Pallotti Hospital', lat: -33.9443605, lng: 18.4900986, maxCapacity: 60 },
+    { id: "004", name: 'Nedbank Salt River Cape', lat: -33.931401, lng: 18.4550689, maxCapacity: 60 },
+    { id: "005", name: 'Nedbank Grand Central', lat: -33.923951, lng: 18.422884, maxCapacity: 60 },
+    { id: "006", name: 'Nedbank Foreshore', lat: -33.91724, lng: 18.4272859, maxCapacity: 60 },
+    { id: "007", name: 'Department of Home Affairs Cape Town', lat: -33.92765, lng: 18.4221, maxCapacity: 60 },
+    { id: "008", name: 'Department of Home Affairs Office', lat: -33.9012178, lng: 18.63154910000003, maxCapacity: 60 },
+    { id: "009", name: 'Home Affairs Wynberg District', lat: -34.0032484, lng: 18.4700239, maxCapacity: 60 },
 ]
-
-// locations: location[] = [
-//     {
-//       lat: -33.9217137,
-//       lng: 18.46171849999996,
-//       name: 'Groote Schuur Hospital',
-//     }, {
-//       lat: -33.9423756,
-//       lng: 18.463247300000035,
-//       name: 'Netcare UCT Private Academic Hospital',
-//     }, {
-//       lat: -33.9443605,
-//       lng: 18.49009860000001,
-//       name: 'Life Vincent Pallotti Hospital',
-//     }, {
-//       lat: -33.931401,
-//       lng: 18.45506899999998,
-//       name: 'Nedbank Salt River Cape',
-//     }, {
-//       lat: -33.923951,
-//       lng: 18.422884000000067,
-//       name: 'Nedbank Grand Central',
-//     },
-//     {
-//       lat: -33.91724,
-//       lng: 18.42728599999998,
-//       name: 'Nedbank Foreshore',
-//     }, {
-//       lat: -33.92765,
-//       lng: 18.4221,
-//       name: 'Department of Home Affairs Cape Town',
-//     }, {
-//       lat: -33.9012178,
-//       lng: 18.63154910000003,
-//       name: 'Department of Home Affairs Office',
-//     }, {
-//       lat: -34.0032484,
-//       lng: 18.4700239,
-//       name: 'Home Affairs Wynberg District',
-//     }]

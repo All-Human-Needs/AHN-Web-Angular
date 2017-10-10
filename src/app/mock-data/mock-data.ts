a35a0bd5bd274f80128d8178a266f3ce418bdc0e
K9-98 <k9idas@gmail.com>
2017-10-10 12:02:17 +0200

    Commit 18
    removed locations array and populated a new locations are with business mock data

--- src/app/mock-data/mock-data.ts
+++ src/app/mock-data/mock-data.ts
@@ -16,6 +16,46 @@ export const Users:User[] = [
 ]
 
 export const RegisteredBusinesses:Business[] = [
-    {id:"ad01c",name:"Tester",lat:0,lng:0,maxCapacity:60},
+    {id:"ad02b",name:"Tester",lat:0,lng:0,maxCapacity:60},
+    {id:"",name:'Groote Schuur Hospital',lat: -33.9217137,lng: 18.46171849999996,maxCapacity:60}
 ]
 
+// locations: location[] = [
+//     {
+//       lat: -33.9217137,
+//       lng: 18.46171849999996,
+//       name: 'Groote Schuur Hospital',
+//     }, {
+//       lat: -33.9423756,
+//       lng: 18.463247300000035,
+//       name: 'Netcare UCT Private Academic Hospital',
+//     }, {
+//       lat: -33.9443605,
+//       lng: 18.49009860000001,
+//       name: 'Life Vincent Pallotti Hospital',
+//     }, {
+//       lat: -33.931401,
+//       lng: 18.45506899999998,
+//       name: 'Nedbank Salt River Cape',
+//     }, {
+//       lat: -33.923951,
+//       lng: 18.422884000000067,
+//       name: 'Nedbank Grand Central',
+//     },
+//     {
+//       lat: -33.91724,
+//       lng: 18.42728599999998,
+//       name: 'Nedbank Foreshore',
+//     }, {
+//       lat: -33.92765,
+//       lng: 18.4221,
+//       name: 'Department of Home Affairs Cape Town',
+//     }, {
+//       lat: -33.9012178,
+//       lng: 18.63154910000003,
+//       name: 'Department of Home Affairs Office',
+//     }, {
+//       lat: -34.0032484,
+//       lng: 18.4700239,
+//       name: 'Home Affairs Wynberg District',
+//     }]
